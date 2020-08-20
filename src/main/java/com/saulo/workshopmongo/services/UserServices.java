c914028ea2a0a47d96351c3b62d584703ddf1ab6
Saulo Godoy Proetti <sauloproetti@gmail.com>
2020-08-20 13:44:32 -0300

    obtendo usuario por id

--- src/main/java/com/saulo/workshopmongo/services/UserServices.java
+++ src/main/java/com/saulo/workshopmongo/services/UserServices.java
@@ -2,10 +2,12 @@ package com.saulo.workshopmongo.services;
 
 import com.saulo.workshopmongo.domain.User;
 import com.saulo.workshopmongo.repository.UserRepository;
+import com.saulo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServices {
@@ -15,5 +17,9 @@ public class UserServices {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
