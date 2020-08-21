18d04830d2b72bf90fa7c1ffd4d017ae58b8b12e
Saulo Godoy Proetti <sauloproetti@gmail.com>
2020-08-21 16:08:58 -0300

    authorDTO

--- src/main/java/com/saulo/workshopmongo/config/Instantiations.java
+++ src/main/java/com/saulo/workshopmongo/config/Instantiations.java
@@ -2,6 +2,7 @@ package com.saulo.workshopmongo.config;
 
 import com.saulo.workshopmongo.domain.Post;
 import com.saulo.workshopmongo.domain.User;
+import com.saulo.workshopmongo.dto.AuthorDTO;
 import com.saulo.workshopmongo.repository.PostRepository;
 import com.saulo.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +33,14 @@ public class Instantiations implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "a@gmaail.com");
         User bob = new User(null, "Bob grey", "b@gmaail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2021"), "Fogo no parquinho", "Pegou fogo no parquinho ontem de noite", maria);
-        Post post2 = new Post(null, sdf.parse("21/04/2021"), "Gelo no parquinho", "Caiu neve no parquinho ontem de noite", maria);
-
         userRepository.save(maria);
         userRepository.save(alex);
         userRepository.save(bob);
+
+        Post post1 = new Post(null, sdf.parse("21/03/2021"), "Fogo no parquinho", "Pegou fogo no parquinho ontem de noite", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/04/2021"), "Gelo no parquinho", "Caiu neve no parquinho ontem de noite", new AuthorDTO(maria));
+
+
 
         postRepository.save(post1);
         postRepository.save(post2);
