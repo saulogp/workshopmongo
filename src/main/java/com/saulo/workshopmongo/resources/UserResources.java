ef32385a13c58dac2438ce9dfe2cfe5e9d6ca3ac
Saulo Godoy Proetti <sauloproetti@gmail.com>
2020-08-21 13:15:15 -0300

    inserção com post

--- src/main/java/com/saulo/workshopmongo/resources/UserResources.java
+++ src/main/java/com/saulo/workshopmongo/resources/UserResources.java
@@ -5,12 +5,11 @@ import com.saulo.workshopmongo.dto.UserDTO;
 import com.saulo.workshopmongo.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,4 +33,13 @@ public class UserResources {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO);
+        obj=service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
