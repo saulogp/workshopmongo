f7746f58595112eb6873db53b8641871d70783dc
Saulo Godoy Proetti <sauloproetti@gmail.com>
2020-08-23 12:47:07 -0300

    consulta com varios parametros

--- src/main/java/com/saulo/workshopmongo/resources/PostResources.java
+++ src/main/java/com/saulo/workshopmongo/resources/PostResources.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.saulo.workshopmongo.resources.util.URL.convertDate;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -32,6 +35,18 @@ public class PostResources {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value="/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date min = convertDate(minDate, new Date(0L));
+        Date max = convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 
